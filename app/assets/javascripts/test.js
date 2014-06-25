dce6ae998626e0827ba3c0fcb53b89428fd8e7c4
Corey <cspeisman@capterra.com>
2014-06-25 16:36:58 -0400

    drawling with jquery

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,3 +1,9 @@
 $.get('/', function(data){
-  var response = data;
+  var response = data
+  $(response).each(function(index, element){
+    width = element[1] + 'px';
+    $('.chart').append('<div></div>');
+    $('div').width(width)
+  })
 });
+
