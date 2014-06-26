474961ce26e19934a141ff2823e196f5b29771bd
Cspeisman <Cspeisman@gmail.com>
2014-06-25 22:30:34 -0400

    added script and style files

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,16 @@
+(function(){
+  var routeData = function(){
+    if ($(this).find('.stop-info').hasClass('hide')){
+      $(this).find('.stop-info').removeClass('hide');
+      $(this).find('.stop-info').slideDown();
+    } else {
+      $(this).find('.stop-info').addClass('hide')
+      $(this).find('.stop-info').slideUp();
+    }
+  }
+
+  $('.stop-info').hide();
+  $('.cell').on('click', routeData);
+
+})();
+
