20a167dd5df5968e9f37e008856625644e2dce89
Cspeisman <Cspeisman@gmail.com>
2014-06-25 22:30:06 -0400

    wrote in ruby and a little jquery

--- app/assets/javascripts/test.js
+++
@@ -1,9 +0,0 @@
-$.get('/', function(data){
-  var response = data
-  $(response).each(function(index, element){
-    width = element[1] + 'px';
-    $('.chart').append('<div></div>');
-    $('div').width(width)
-  })
-});
-
--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,6 +1,5 @@
 class BusStopsController < ApplicationController
   def index
     @routes = BusStop.get_route_stops_num
-    render json: @routes if request.xhr?
   end
 end
